28a970c318ec76736e080f9685a10eeec7222256
Alen Jose Anto <alen.anto@fau.de>
2024-11-08 16:08:30 +0100

    Made repository pip installable by adding setup.py and other files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version='1',
+    description="A math quiz ",
+    author="Sharon Varghese",
+    packages=find_packages(),
+    install_requires=[],  
+)
